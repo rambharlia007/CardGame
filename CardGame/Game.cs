25879888bc662e0dca1be459d7c435f5adb8c00a
Ram Bharlia <ram.b@deltax.com>
2018-01-08 18:14:29 +0530

    My First COmmit

---
+++ CardGame/Game.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+   public class Game
+   {
+        // cards game have always 52 cards
+       private readonly string[] _cardsVale = new string[]
+       {
+           "Spades_A", "Spades_2", "Spades_3", "Spades_4", "Spades_5", "Spades_6", "Spades_7", "Spades_8", "Spades_9","Spades_K", "Spades_Q", "Spades_J",
+           "Hearts_A", "Hearts_2", "Hearts_3", "Hearts_4", "Hearts_5", "Hearts_6", "Hearts_7", "Hearts_8", "Hearts_9","Hearts_K", "Hearts_Q", "Hearts_J",
+           "Diamonds_A", "Diamonds_2", "Diamonds_3", "Diamonds_4", "Diamonds_5", "Diamonds_6", "Diamonds_7", "Diamonds_8", "Diamonds_9","Diamonds_K", "Diamonds_Q", "Diamonds_J",
+           "Ace_A","Ace_2", "Ace_3", "Ace_4", "Ace_5", "Ace_6", "Ace_7", "Ace_8", "Ace_9", "Ace_K","Ace_Q", "Ace_J"
+       };
+
+       public Queue<string> Cards;
+        public Game()
+        {
+            Cards = new Queue<string>(_cardsVale);
+        }
+
+        public string Play()
+        {
+           return Cards.Dequeue();
+        }   
+
+        public void Shuffle()
+        {
+            var tempArray = Cards.ToArray();
+            new Random().Shuffle(tempArray);
+            Cards = new Queue<string>(tempArray);
+        }
+
+        public void Restart()
+        {
+            Cards = new Queue<string>(_cardsVale);
+        }
+    }
+
+
+    public enum GameOptions
+    {
+        Play = 1,
+        Shuffle = 2,
+        Restart = 3,
+        Quit = 4
+    }
+
+
+}
--- CardGame/Program.cs
+++ CardGame/Program.cs
@@ -1,15 +1,80 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CardGame
 {
-    class Program
+   public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Options available");
+            Console.WriteLine("enter 1 to play game");
+            Console.WriteLine("enter 2 to shufflr card");
+            Console.WriteLine("enter 3 to play again");
+
+            var game = new Game();
+            game.Shuffle();
+            var isQuiteGame = false;
+            do
+            {
+                var inputString = Console.ReadLine();
+                var isInputValid = ValidateInput(inputString);
+                
+                if (isInputValid)
+                {
+                    var numericInput = Int32.Parse(inputString);
+                    isQuiteGame = numericInput == (int) GameOptions.Quit;
+                    bool isGameOver = IsGameOver(numericInput, game);
+
+                    if (isGameOver)
+                    {
+                        Console.WriteLine("Game over, do you want to restart");
+                    }
+                    else
+                        StartGame(numericInput, game);
+                }
+                else
+                {
+                    Console.WriteLine("Input is not valid");
+                }
+            } while (!isQuiteGame);
+        }
+
+        private static bool IsGameOver(int numericInput, Game game)
+        {
+            return game.Cards.Count == 0 &&
+                   (numericInput == (int) GameOptions.Play || numericInput == (int) GameOptions.Shuffle);
+        }
+
+        private static void StartGame(int numericInput, Game game)
+        {
+            switch (numericInput)
+            {
+                case (int)GameOptions.Play:
+                {
+                    Console.WriteLine(game.Play());
+                    break;
+                }
+                case (int) GameOptions.Shuffle:
+                {
+                    game.Shuffle();
+                    Console.WriteLine("Shuffle Done");
+                    break;
+                }
+                case (int) GameOptions.Restart:
+                {
+                    game.Restart();
+                    Console.WriteLine("Game restarted");
+                    break;
+                }
+            }
+        }
+
+
+        private static bool ValidateInput(string inputString)
+        {
+            var validInput = new[] {"1", "2", "3", "4"};
+            return validInput.Contains(inputString);
         }
     }
 }
